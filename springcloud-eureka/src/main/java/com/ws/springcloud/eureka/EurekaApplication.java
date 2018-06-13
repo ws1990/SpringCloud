1eac0fd08a290593dbb32a3a74caf20808618985
王松 <576023219@qq.com>
2018-06-13 14:19:26 +0800

    提交人：王松 提交内容：搭建框架——eureka

---
+++ springcloud-eureka/src/main/java/com/ws/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,22 @@
+package com.ws.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description 
+ * 
+ * @author 王松
+ * @date 2018/6/13 11:45
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
