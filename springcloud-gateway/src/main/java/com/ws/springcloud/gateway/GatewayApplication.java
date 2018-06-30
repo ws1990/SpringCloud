5dc443b06705027d340f16d21231096915eaeea4
王松 <576023219@qq.com>
2018-06-30 16:18:58 +0800

    提交人：王松 提交内容：添加gateway

---
+++ springcloud-gateway/src/main/java/com/ws/springcloud/gateway/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.ws.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @description 
+ * 
+ * @author 王松
+ * @date 2018/6/30 15:42
+ * @version 1.0
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
