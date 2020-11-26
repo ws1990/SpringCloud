b0ec3dacd29485bcf5ebf7b9cbd8c7d5e1228930
wangsong <wheihei@qq.com>
2020-11-26 11:26:40 +0800

    [registry] 允许跨域

---
+++ springcloud-registry/src/main/java/com/ws/springcloud/eureka/config/RegisterWebSecurityConfigure.java
@@ -0,0 +1,21 @@
+package com.ws.springcloud.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Description：
+ *
+ * @author wangsong
+ * @version 1.0
+ * @since 11/26/2020 11:21 AM
+ */
+@EnableWebSecurity
+public class RegisterWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
