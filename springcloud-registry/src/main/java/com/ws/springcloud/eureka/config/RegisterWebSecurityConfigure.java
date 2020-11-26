cc47a4bc06e0f627d27427fb2595edf071267937
wangsong <wheihei@qq.com>
2020-11-26 14:31:36 +0800

    [registry] local环境下，不需要security验证

--- springcloud-registry/src/main/java/com/ws/springcloud/eureka/config/RegisterWebSecurityConfigure.java
+++ springcloud-registry/src/main/java/com/ws/springcloud/eureka/config/RegisterWebSecurityConfigure.java
@@ -1,5 +1,7 @@
 package com.ws.springcloud.eureka.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,9 +15,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @EnableWebSecurity
 public class RegisterWebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private Environment environment;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // 允许其他service注册时跨域访问
         http.csrf().ignoringAntMatchers("/eureka/**");
-        super.configure(http);
+
+        // local环境下，不需要登录验证
+        // 其他环境下，必须验证
+        if (isLocalEnv()) {
+            http.authorizeRequests()
+                    .anyRequest().permitAll().and().logout().permitAll();
+        } else {
+            super.configure(http);
+        }
+    }
+
+    private boolean isLocalEnv() {
+        String[] activeProfiles = environment.getActiveProfiles();
+        if (activeProfiles.length == 0) {
+            return false;
+        }
+
+        for (String profile : activeProfiles) {
+            if ("local".equalsIgnoreCase(profile)) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
